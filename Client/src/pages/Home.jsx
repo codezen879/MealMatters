c0c7d195dd7a58b3c0f11f8699d96c32f7c48ec3
Ayush <ayushsingh00030@gmail.com>
2024-03-22 23:09:00 +0530

    Added commit 4

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HeroImage from "../Images/HeroImage2.jpg";
-import MyModal from "./DividerModal"; // Make sure to import the Modal component
 
-const Home = () => {
-    const [modalOpen, setModalOpen] = useState(false);
+import { Button } from '@/components/ui/button'
 
-    const handleOpenModal = () => {
-      setModalOpen(true);
-    };
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
   
-    const handleCloseModal = () => {
-      setModalOpen(false);
-    };
 
+const Home = () => {    
     return (
         <div name="home" className="w-full bg-gradient-to-b">
             <div className="relative">
@@ -23,21 +24,28 @@ const Home = () => {
                     alt=""
                 />
                 <div className="absolute flex flex-col justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    {/* <Link
-                        to="/register"
-                        className=""
-                    >
-                        Get Started
-                    </Link> */}
-                    <button
-                        className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 mt-80 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
-                        onClick={handleOpenModal}
-                    >
-                        Get Started
-                    </button>
+                
+                    <Dialog>
+                    <DialogTrigger asChild>
+                        <Button
+                            className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 mt-80 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
+                        >
+                            Get Started
+                        </Button>
+                    </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                <DialogDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                                </DialogDescription>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+
                 </div>
             </div>
-            <MyModal />
         </div>
     );
 };
