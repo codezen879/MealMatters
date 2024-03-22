d068acfc6060a12acb889cce5238292490f170da
BaldwinThe4 <sanchitcodecrafter@gmail.com>
2024-03-22 15:42:42 +0530

    foodDonation Schema

---
+++ Server/models/foodDonation.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const foodDonationSchema = new mongoose.Schema(
+  {
+    donorID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    foodType: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    expirationDate: {
+      type: Date,
+      required: true,
+    },
+    pickupAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    deliveryAvailable: {
+      type: Boolean,
+      default: false,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Accepted", "Rejected", "Completed"],
+      default: "Pending",
+    },
+    
+  },
+  { timestamps: true }
+);
+
+const FoodDonation = mongoose.model("FoodDonation", foodDonationSchema);
+
+module.exports = FoodDonation;
